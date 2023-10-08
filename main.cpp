ce6632ec8c1e57cd17a7d986f524c7bee5c15f73
Elisa Medina <147352316+KittiePower@users.noreply.github.com>
2023-10-08 16:45:40 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,34 @@
+/******************************************************************************
+Declarar arreglos 
+Autor: Elisa Michelle MV.
+Fecha: 3003322
+******************************************************************************/
+#include <iostream>
+using namespace std;
+int main ()
+{
+    //Declaración de variables y arreglos 
+int numeros [5]= (4,10,33,104,110);
+//numeros que se va a sumar 
+int numerosolicitado;
+//arreglo donde guardo los resultados 
+int suma [5];
+
+//Realiza la suma y guardar en el arreglo suma;
+cout<<"escribe el numero a sumar";
+cin>>numerosolicitado;
+suma [0]=numeros [0]+numerosolicitado;
+suma [1]=numeros [1]+numerosolicitado;
+suma [2]=numeros [2]+numerosolicitado;
+suma [3]=numeros [3]+numerosolicitado;
+suma [4]=numeros [4]+numerosolicitado;
+//Enviar resultado a pantalla 
+cout<< "Arreglo suma posicion 0:" <<suma [0]<<endl;
+cout<< "Arreglo suma posicion 1:" <<suma [1]<<endl;
+cout<< "Arreglo suma posicion 2:" <<suma [2]<<endl;
+cout<< "Arreglo suma posicion 3:" <<suma [3]<<endl;
+cout<< "Arreglo suma posicion 4:"<<suma [4]<<endl;
+    return 0;
+    return 0;
+
+}
